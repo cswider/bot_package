d5233ddb9b2a6e27f122fc65a30836ce58e1d71e
cswider <christopher.swider@gmail.com>
2020-09-26 13:37:25 -0700

    Create parameters.py

---
+++ 3080_bot/parameters.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Sep 25 20:07:12 2020
+
+@author: chris
+"""
+
+# Product direct URL (pick one)
+product_URL = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-2070-super-8gb-gddr6-pci-express-3-0-graphics-card-black-silver/6361328.p?skuId=6361328'
+#product_URL = 'https://www.bestbuy.com/site/nvidia-geforce-rtx-3080-10gb-gddr6x-pci-express-4-0-graphics-card-titanium-and-black/6429440.p?skuId=6429440'
+
+# Quantity to purchase
+QTY_to_buy = 3
+
+# BestBuy account username and password (password in plain text idk encryption seems like too much work)
+email = 'yourAccount@email.com'
+password = 'Password'
+
+# Shipping info
+shipping_first_name = 'Firstname'
+shipping_last_name = 'Lastname'
+shipping_address = '1111 W Example St'
+shipping_city = 'City'
+shipping_state = 'AZ'
+shipping_zip = '99999'
+phone_number = '8085555555'
+
+# Payment info
+CC_number = '6011653024337180', # fake number for testing purposes
+billing_first_name = 'Firstname'
+billing_last_name = 'Lastname'
+billing_address = '##-## Example Pl Apt 1'
+billing_city = 'City'
+billing_state = 'HI'
+billing_zip = '99999'
